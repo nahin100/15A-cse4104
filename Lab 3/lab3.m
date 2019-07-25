7dd0c0c220cfba5b0d1096df5c8a2826a5146008
Nahin Ul Sadad <12844583+nahinulsadad@users.noreply.github.com>
2019-07-25 18:33:03 +0600

    Update lab3.m

--- Lab 3/lab3.m
+++ Lab 3/lab3.m
@@ -1,37 +1,3 @@
-%Signal Addition and Multiplication
-n1=[-1,0,1];
-x1=[5,4,3];
-
-n2=[-2,-1,0,1,2,3];
-x2=[1,2,3,4,7,8,9];
-
-m1 = min([n1,n2]);
-m2 = max([n1,n2]);
-
-m = m1:m2;
-
-newx1= zeros(1, length(m));
-newx2= zeros(1, length(m));
-y = zeros(1, length(m));
-
-for i = 1:length(m)
-    for j = 1:length(n1)
-        if m(i)==n1(j)
-            newx1(i)=x1(j);
-        end
-    end
-end
-
-for i = 1:length(m)
-    for j = 1:length(n2)
-        if m(i)==n2(j)
-            newx2(i)=x2(j);
-        end
-    end
-end
-
-y = newx1 + newx2;
-
 %Signal Shifting
 n2=[-3,-2,-1,0,1,2,3];
 x2=[1,2,3,4,7,8,9];
@@ -65,7 +31,7 @@ nye = nx(length(x)) + nh(length(h));
 ny = [nybx:nye];
  
 
-% Frequency analysis ploting
+% Frequency analysis ploting (Homework 1)
 w = [0:1:500]*pi/500; % [0, pi] axis divided into 501 points.
 X = exp(j*w)+2+3.*exp(-j*w)+4.*exp(-j*2*w)+5.*exp(-j*3*w);
 magX = abs(X); angX = angle(X); realX = real(X); imagX = imag(X);
